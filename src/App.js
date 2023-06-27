5cfb58026f8c098aa7a230ed007ed1b2632fd965
Prachi24599 <prachi24599@gmail.com>
2023-06-27 12:14:21 +0530

    Route created for different path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/support" element={<div>Support Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/labs" element={<div>Labs Page</div>} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
